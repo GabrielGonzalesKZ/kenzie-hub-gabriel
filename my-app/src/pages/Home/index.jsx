b9ab3d1c2941d141439776fa0b18240c031b688f
Gabriel Gonzales <106750788+GabrielGonzalesKZ@users.noreply.github.com>
2022-12-15 09:23:39 -0300

    fix: limpando localstorage

--- my-app/src/pages/Home/index.jsx
+++ my-app/src/pages/Home/index.jsx
@@ -6,12 +6,13 @@ import { StyledHome } from "./styled"
 export const Home = () => {
 
     const { user } = useContext(AuthContext)
+    const deletToken = localStorage.removeItem("@TOKEN");
 
     return (
         <StyledHome className="animate__animated animate__fadeInDown">
             <div className="nav">
                 <h1>Kenzie Hub</h1>
-                <button>{user && <Link to="/login">Sair</Link>}</button>
+                <button onClick={deletToken}>{user && <Link to="/login">Sair</Link>}</button>
             </div>
             <div className="user">
                 <div className="text">
